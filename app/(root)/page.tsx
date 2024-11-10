58a765eb72dc56b0502e2e1be506001ed494ec15
hoshikwyw <khaingwutyiwin1712@gmail.com>
2024-11-10 16:04:42 +0630

    Search form added

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,16 @@
+import SearchForm from "@/components/SearchForm";
 
-export default function Home() {
+export default async function Home({searchParams}: {
+  searchParams: Promise<{ query: string }>
+}) {
+    const query = (await searchParams).query
+
   return (
     <>
     <section className="pink_container">
       <h1 className="heading">Collection of the news of Civil War in Myanmar</h1>
       <p className="sub-heading !max-w-3xl">What happening in Myanmar during the Civil War</p>
+      <SearchForm query={query} />
     </section>
     </>
   );
