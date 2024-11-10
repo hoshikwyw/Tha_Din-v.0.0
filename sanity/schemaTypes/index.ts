97a0343c13b947110689bb33ad58615e620177b1
hoshikwyw <khaingwutyiwin1712@gmail.com>
2024-11-10 22:37:07 +0630

    Sanity schema added for author and new

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,5 +1,7 @@
 import { type SchemaTypeDefinition } from 'sanity'
+import { author } from './author'
+import { news } from './news'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [],
+  types: [author,news],
 }
--- sanity/structure.ts
+++ sanity/structure.ts
@@ -4,5 +4,8 @@ import type {StructureResolver} from 'sanity/structure'
 export const structure: StructureResolver = (S) =>
   S.list()
     .title('Content')
-    .items(S.documentTypeListItems())
+    .items([
+      S.documentTypeListItem("author").title("Authors"),
+      S.documentTypeListItem("news").title("News")
+    ])
 
